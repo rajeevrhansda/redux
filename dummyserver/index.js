600f0c99c04342216082cdc9a09d7f4d8d894703
Rajeev Hansda <rajeevrhansda@gmail.com>
2022-07-30 08:36:45 +0530

    dummy server

---
+++ dummyserver/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/user/:id/update', (req, res)=>{
+    setTimeout(()=>{
+        res.send(req.body);
+    },[2000]);
+});
+
+app.listen(8800, ()=>{
+    console.log("Backend server started");
+})
